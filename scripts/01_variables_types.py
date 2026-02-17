b8649572c2b227314b8b26e52f5c806df8b8f6b3
Feda Mohammadi <mohammadif@berea.edu>
2026-02-17 13:18:46 -0500

    second part

--- scripts/01_variables_types.py
+++ scripts/01_variables_types.py
@@ -28,6 +28,22 @@ def main() -> None:
     print("This lesson will introduce Python variables and basic data types.")
 
 
+def main() -> None:
+    section("1) Variables and Basic Types")
+
+    age = 21                  # int
+    gpa = 3.65                # float
+    name = "Feda"             # str
+    is_student = True         # bool
+
+    print(f"age = {age} (type: {type(age)})")
+    print(f"gpa = {gpa} (type: {type(gpa)})")
+    print(f"name = {name} (type: {type(name)})")
+    print(f"is_student = {is_student} (type: {type(is_student)})")
+
+
+
+
 if __name__ == "__main__":
     main()
 
