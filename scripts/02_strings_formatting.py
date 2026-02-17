35d82e4a448510f0ac4208e4b1fc902af883e983
Feda Mohammadi <mohammadif@berea.edu>
2026-02-17 13:58:54 -0500

    ===

--- scripts/02_strings_formatting.py
+++ scripts/02_strings_formatting.py
@@ -5,6 +5,6 @@ set_b = {2, 4, 8, 5, 9, 10}
 print(set_a.union(set_b))
 print(set_a | set_b)
 
-#This way, you can print all the duplicated or intersaction numbers.
+
 print(set_a & set_b)
 print(set_a.intersection(set_b))
