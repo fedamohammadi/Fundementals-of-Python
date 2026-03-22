688594b5b2594ceb0df338b872f801aa033da281
Feda Mohammadi <mohammadif@berea.edu>
2026-03-21 23:23:15 -0400

    Added OOP minimum for research

---
+++ scripts/14_oop_minimum_for_research.py
@@ -0,0 +1,25 @@
+"""
+- Defining a class
+- Creating objects
+- Instance attributes
+- Instance methods
+- __init__
+- __repr__
+
+"""
+
+def section(title: str) -> None:
+    print("\n" + "=" * 60)
+    print(title)
+    print("=" * 60)
+
+
+
+def main() -> None:
+    section("OOP Minimum for Research")
+ 
+
+
+
+if __name__ == "__main__":
+    main()
